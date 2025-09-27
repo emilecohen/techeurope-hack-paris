b051da6306fad48d1d2304ed99396c420d4e8ced
Jose Antonio Contreras Ortiz <jose.contreras@itj.com>
2025-09-27 23:34:08 +0200

    feat: agent changes

--- agent.py
+++ agent.py
@@ -12,53 +12,74 @@ from livekit.plugins import (
     openai,
     noise_cancellation,
 )
+from livekit.agents import mcp
+from fastmcp import Client
 
 
 load_dotenv(".env.local")
 
 
-class Assistant(Agent):
-    def __init__(self) -> None:
-        super().__init__(instructions="You are a helpful voice AI assistant.")
-
-    async def on_enter(self) -> None:
-        await self.session.generate_reply(
-            instructions="Greet the user and ask how you can help them."
+@function_tool()
+async def set_up_times_agent(context: RunContext) -> dict[str]:
+    """Transfer the call to the New York Times Agent, whenever the user mention something can"""
+    client = Client("https://techeurope-hack-pari-6f861422.alpic.live/")
+    async with client:
+        result = await client.call_tool(
+            "get_config_instructions",
         )
 
-    @function_tool()
-    async def lookup_weather(
-        self,
-        context: RunContext,
-        location: str,
-    ) -> dict[str]:
-        """Look up weather information for a given location.
+    return (
+        Assistant(
+            instructions=result.content[0].text, greet="Times", agentType="Times"
+        ),
+        "Transferring to The New York Times support",
+    )
 
-        Args:
-            location: The location to look up weather information for.
-        """
 
-        return {"weather": "sunny", "temperature_f": 70}
+@function_tool()
+async def revert_to_base_agent(context: RunContext) -> dict[str]:
+    """Revert the agent call to original settings"""
+    return (
+        Assistant(),
+        "Transferring to Base Agent",
+    )
+
+
+class Assistant(Agent):
+    def __init__(
+        self, instructions: str = None, greet: str = None, agentType: str = None
+    ) -> None:
+        super().__init__(
+            instructions=(
+                "You are a helpful voice AI assistant that speaks english."
+                if instructions is None
+                else instructions
+            ),
+            tools=[set_up_times_agent] if agentType is None else [revert_to_base_agent],
+        )
+        self.greet = (
+            "Greet the user and ask how you can help them."
+            if greet is None
+            else "Introduce yourself as a The New York Times specialist and ask how you can help with their account always mention what tools you have avalible."
+        )
+
+    async def on_enter(self) -> None:
+        await self.session.generate_reply(instructions=self.greet)
 
 
 async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
         llm=openai.realtime.RealtimeModel(voice="coral"),
-        # mcp_servers=[mcp.MCPServerHTTP("http://localhost:8000/mcp")],
+        mcp_servers=[mcp.MCPServerHTTP("http://localhost:8000/mcp")],
     )
 
     await session.start(
         room=ctx.room,
         agent=Assistant(),
         room_input_options=RoomInputOptions(
-            # For telephony applications, use `BVCTelephony` instead for best results
             noise_cancellation=noise_cancellation.BVC(),
         ),
     )
-
-    # await session.generate_reply(
-    #     instructions="Greet the user and offer your assistance. You should start by speaking in English."
-    # )
 
 
 if __name__ == "__main__":
--- mcp/app/main.py
+++ mcp/app/main.py
@@ -19,5 +19,11 @@ def subtract(a: float, b: float) -> float:
     return a - b
 
 
+@mcp.tool
+def speak_faster() -> str:
+    """Gives the model some instructions on how to speak"""
+    return f"Plss speak faster"
+
+
 # Create ASGI application
 app = mcp.http_app()
